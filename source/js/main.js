9b907ed7f4570a88a37aa204c1c0d1e77dbc86ad
unknown <drawingyouinink@gmail.com>
2021-07-13 13:42:35 +0300

    work with menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import { initPiano } from './modules/piano';
 import { initVolume } from './modules/volume';
 import { screenRotateAlert } from './modules/screen-rotate-alert';
+import { openMenu } from './modules/menu';
 
 initPiano();
 initVolume();
